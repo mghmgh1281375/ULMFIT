6eb50aae960fd00893a78fb3bfdaa5c01b785d75
Mohammad <mghmgh1281375@gmail.com>
2019-06-27 16:56:13 +0430

    UPDATE

--- preprocess.py
+++ preprocess.py
@@ -2,17 +2,42 @@
 # Preprocess data using Hazm
 
 import pandas as pd
+import re, os
+from termcolor import colored, cprint
 from hazm import WordTokenizer, Normalizer
 tokenizer = WordTokenizer(separate_emoji=False, replace_links=False, replace_IDs=False, replace_emails=False, replace_numbers=False, replace_hashtags=False)
 normalizer = Normalizer()
 
+def remove_section(filename):
+    pattern = re.compile(r'Section\:+')
+    with open(filename, 'r') as file:
+        with open(os.path.join(os.path.dirname(filename), f'without_section.{os.path.basename(filename)}'), 'w') as out:
+            line = file.readline()
+            while line:
+                out.write(pattern.sub(string=line, repl=''))
+                line = file.readline()
 
+    return os.path.join(os.path.dirname(filename), f'without_section.{os.path.basename(filename)}')
 
 if __name__ == "__main__":
-    df = pd.read_csv('resources/fa.wiki.csv')
-    # print(df.columns)
-    print(df.head(), len(df))
-    df = df.apply(lambda x: normalizer.normalize(x.tolist()[0]), axis=1)
-    print(df.head(), len(df))
-    df.to_csv('resources/fa.wiki.normalized.csv')
 
+    # Remove Section:::: 
+    # filname = remove_section('resources/fa.wiki.small.csv')
+    filname = remove_section('resources/fa.wiki.csv')
+    # df = pd.read_csv(filname)#, nrows=20)
+
+    # # Normalize
+    # df = pd.read_csv('resources/fa.wiki.csv')#, nrows=20)
+    # print(df.columns, df.head(1), len(df))
+    # s = df.apply(lambda x: normalizer.normalize(x.tolist()[0]), axis=1)
+    # print(s.head(1), len(s))
+    # s.to_csv('resources/fa.wiki.normalized.csv', header=['text'], index=False)
+
+    # # Tokenize
+    # df = pd.read_csv('resources/fa.wiki.normalized.csv')#, nrows=20)
+    # print(df.columns, df.head(1), len(df))
+    # s = df.apply(lambda x: ' '.join(tokenizer.tokenize(x.tolist()[0])), axis=1)
+    # print(s.head(1), len(s))
+    # s.to_csv('resources/fa.wiki.normalized.tokenized.csv', header=['text'], index=False)
+
+    print(colored('Successful!', 'green', attrs=['reverse', 'blink']))
--- train_cls.py
+++ train_cls.py
@@ -1,53 +1,42 @@
 
 from fastai.text import *
-from fastai import *
+from utils import get_text_representation, _model_meta
+from train_utils import train_lm, train_clf
 
-_model_meta = {AWD_LSTM: {'hid_name': 'emb_sz', 'url': URLs.WT103_1,
-                          'config_lm': awd_lstm_lm_config, 'split_lm': awd_lstm_lm_split,
-                          'config_clas': awd_lstm_clas_config, 'split_clas': awd_lstm_clas_split},
-               Transformer: {'hid_name': 'd_model', 'url': URLs.OPENAI_TRANSFORMER,
-                             'config_lm': tfmer_lm_config, 'split_lm': tfmer_lm_split,
-                             'config_clas': tfmer_clas_config, 'split_clas': tfmer_clas_split},
-               TransformerXL: {'hid_name': 'd_model',
-                               'config_lm': tfmerXL_lm_config, 'split_lm': tfmerXL_lm_split,
-                               'config_clas': tfmerXL_clas_config, 'split_clas': tfmerXL_clas_split}}
-
-data_lm = (TextList.from_csv('resources', 'partial-train.csv')
+# # # Data section
+data_lm = (TextList.from_csv('resources', 'fa.wiki.small.csv')
                    .split_by_rand_pct()
                    .label_for_lm()
                    .databunch())
-# data_lm.save()
-# data_lm.show_batch()
-
-# TODO: Here i can fine-tune lm with smaller data
-
-# learn = language_model_learner(data_lm, AWD_LSTM)
-# learn.fit_one_cycle(1, 1e-2)
-# learn.save('best_lm')
-# learn.save_encoder('best_enc')
-
-# learn.show_results()
-
-data_clas = (TextList.from_csv('resources', 'fa.train.csv', cols='text', vocab=data_lm.vocab)
+                   
+data_clas = (TextList.from_csv('resources', 'fa.digikala.csv', cols='text', vocab=data_lm.vocab)
              .split_subsets(train_size=0.7, valid_size=0.3)
              .label_from_df(cols='data')
              .databunch(bs=16))
+
+# data_lm.save()
+# data_lm = pickle.load(open('resources/data_save.pkl', 'rb'))
+# data_lm.show_batch()
 # data_clas.show_batch()
+print('LM vocab size', len(data_lm.vocab.itos),
+', CLS vocab size', len(data_clas.vocab.itos),
+', CLS n_class', data_clas.c)
+
+# # # Train LM
+lm_learner = train_lm()
+
+# # # Fine-tuning language model section
+# lm_model = get_language_model(AWD_LSTM, len(
+#     data_lm.vocab.itos), config=None, drop_mult=1.)
+# meta = _model_meta[AWD_LSTM]
+# lm_learn = LanguageLearner(data_clas, lm_model, split_func=meta['split_lm'])
+# fnames = [learn.path/learn.model_dir/f'{fn}.{ext}' for fn,ext in zip(pretrained_fnames, ['pth', 'pkl'])]
+# learn.load_pretrained(*fnames)
+# lm_learn.freeze()
 
 
-# # TODO: Load lm and fine-tune with classification data
-# model = get_language_model(arch, len(data.vocab.itos), config=config, drop_mult=drop_mult)
-# meta = _model_meta[arch]
-# learn = LanguageLearner(data, model, split_func=meta['split_lm'], **learn_kwargs)
+# # # Classification section
+cls_learner = train_clf()
+print(cls_learner.summary())
+print(get_text_representation(cls_learner, "سلام چطوری؟"))
 
-
-
-model = get_text_classifier(AWD_LSTM, len(data_lm.vocab.itos), data_clas.c, bptt=70, max_len=70*20,
-                            config=None, drop_mult=1., lin_ftrs=None, ps=None)
-meta = _model_meta[AWD_LSTM]
-learn = RNNLearner(data_clas, model, split_func=meta['split_clas'])
-
-learn.load_encoder('best_enc')
-learn.fit_one_cycle(2, slice(1e-3, 1e-2))
-learn.save('mini_train_clas')
-learn.show_results()
